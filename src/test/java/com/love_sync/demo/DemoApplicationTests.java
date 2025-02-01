d23ef823eb7d8277b8f4798ca32eec11f818d454
askarov32 <insar@lessons.kz>
2025-02-01 20:23:17 +0500

    initial commit

---
+++ src/test/java/com/love_sync/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love_sync.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
